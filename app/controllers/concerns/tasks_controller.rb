212f988d596707c2f95495737efc23f5b5e90ccd
Marie Menjucq <mariemnjc@gmail.com>
2025-03-04 17:58:37 +0100

    task manager

---
+++ app/controllers/concerns/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_path)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+end
